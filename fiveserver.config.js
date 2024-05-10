cf301978173e0bb289538a441cb663189df5443a
Historical22 <168996108+Historical22@users.noreply.github.com>
2024-05-10 15:38:22 -0400

    Add files via upload

---
+++ fiveserver.config.js
@@ -0,0 +1,10 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: true, // enable remoteLogs
+    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+    injectCss: false, // disable injecting css
+    navigate: true, // enable auto-navigation
+  };
+  
